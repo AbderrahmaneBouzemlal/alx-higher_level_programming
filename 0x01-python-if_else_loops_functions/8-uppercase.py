bcd1814550960f4531490188872b06b0361377de
AbderrahmaneBouzemlal <Abderrahmanebouzemlal@gmail.com>
2024-01-16 23:25:27 +0800

    Added a file

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+def uppercase(str):
+    for c in str:
+        c = ord(c)
+        if c > 96 or c > 97 + 26:
+            c -= 32
+        c = chr(c)
+        print("{}".format(c), end="")
+    print("")
