72b31090c918ef2d749675a3f9e6473d8012d83d
AbderrahmaneBouzemlal <Abderrahmanebouzemlal@gmail.com>
2024-01-18 20:08:23 +0800

    Added a file

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 import sys
 argc = len(sys.argv)
-if argc == 1:
-    print("0")
-for arg in range(2, argc):
-    argv_int = int(sys.argv[arg])
-    argv_int += argv_int
-print(argv_int)
+if __name__ == "__main__":
+    if argc == 1:
+        print("0")
+    for arg in range(2, argc):
+        argv_int = int(sys.argv[arg])
+        argv_int += argv_int
+    print(argv_int)
