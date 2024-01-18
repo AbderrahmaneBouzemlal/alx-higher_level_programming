231c0ce8e8b60ac5316fb8852bc5aae62564d4b5
AbderrahmaneBouzemlal <Abderrahmanebouzemlal@gmail.com>
2024-01-18 18:25:56 +0800

    Added a file

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,12 +2,13 @@
 import sys
 argc = len(sys.argv)
 argc -= 1
-if argc == 0:
-    print(f"{argc} arguments.")
-elif argc == 1:
-    print(f"{argc} argument:")
-    print(f"1: {sys.argv[1]}")
-else:
-    print(f"{argc} arguments:")
-    for i in range(1, argc + 1):
-        print(f"{i}: {sys.argv[i]}")
+if __name__ == "__main__":
+    if argc == 0:
+        print(f"{argc} arguments.")
+    elif argc == 1:
+        print(f"{argc} argument:")
+        print(f"1: {sys.argv[1]}")
+    else:
+        print(f"{argc} arguments:")
+        for i in range(1, argc + 1):
+            print(f"{i}: {sys.argv[i]}")
