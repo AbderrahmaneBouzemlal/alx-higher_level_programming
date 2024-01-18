c62267075e7fed5fdc83cf7ce0b3728348acbfa9
AbderrahmaneBouzemlal <Abderrahmanebouzemlal@gmail.com>
2024-01-18 18:27:59 +0800

    Added a file

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,6 +2,7 @@
 import calculator_1 as calc
 a = 10
 b = 5
+
 if __name__ == "__main__":
     print("{} + {} = {}".format(a, b, calc.add(a, b)))
     print("{} - {} = {}".format(a, b, calc.sub(a, b)))
