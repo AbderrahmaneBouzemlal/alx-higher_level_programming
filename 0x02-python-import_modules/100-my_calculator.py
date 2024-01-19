e3a8a5edca216acf82c619ae10560e89a1ec3542
AbderrahmaneBouzemlal <Abderrahmanebouzemlal@gmail.com>
2024-01-19 09:46:08 +0800

    Added a file

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    if sys.argv[2] not in "+-*/":
+    elif sys.argv[2] not in "+-*/":
         print("Unknown operator. Available operators: +, -, *, /")
         exit(1)
 
