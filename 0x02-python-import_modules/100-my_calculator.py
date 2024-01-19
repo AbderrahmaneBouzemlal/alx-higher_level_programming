d3909f69e3c7e29753ecfe6ccbe163866268f734
AbderrahmaneBouzemlal <Abderrahmanebouzemlal@gmail.com>
2024-01-19 09:37:55 +0800

    Added a file

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
+    from calculator_1 import add, sub, mul, div
     argc = len(sys.argv)
     if argc != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
@@ -15,10 +16,11 @@ if __name__ == "__main__":
         b = int(sys.argv[3])
 
         if sys.argv[2] == "+":
-            print("{} + {} = {}".format(a, b, a + b))
+            print("{} + {} = {}".format(a, b, add(a, b)))
         elif sys.argv[2] == "-":
-            print("{} - {} = {}".format(a, b, a - b))
+            print("{} - {} = {}".format(a, b, sub(a, b)))
         elif sys.argv[2] == "*":
-            print("{} * {} = {}".format(a, b, a * b))
+            print("{} * {} = {}".format(a, b, mul(a, b)))
         elif sys.argv[2] == "/":
-            print("{} / {} = {}".format(a, b, a / b))
+            print("{} / {} = {}".format(a, b, div(a, b)))
+        exit(0)
