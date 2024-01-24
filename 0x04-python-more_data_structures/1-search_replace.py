4ac9ff8edabd59acf6d13892550fd361365138c5
AbderrahmaneBouzemlal <Abderrahmanebouzemlal@gmail.com>
2024-01-24 22:25:25 +0800

    Added a file

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = my_list[:]
+    for element in range(0, len(new_list) -1):
+        if new_list[element] == search:
+            new_list[element] = replace
+
+    return new_list
