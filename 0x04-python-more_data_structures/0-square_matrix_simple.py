02eb5c03eeb66f23cdfd870a1f28a08e4e9ebe0a
AbderrahmaneBouzemlal <Abderrahmanebouzemlal@gmail.com>
2024-01-24 22:27:00 +0800

    Added a file

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,7 +2,7 @@
 def square_matrix_simple(matrix=[]):
     mul = []
     for i in matrix:
-        resu = map(lambda x : x * x, i)
+        resu = map(lambda x: x * x, i)
         resu = list(resu)
         mul.append(resu)
     return mul
