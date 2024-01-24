fe132283db957e55df6a67fea02f7b5847208535
AbderrahmaneBouzemlal <Abderrahmanebouzemlal@gmail.com>
2024-01-24 21:40:48 +0800

    Added a file

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    square_matrix = map(lambda x: x * x, matrix)
+    return square_matrix
---
+++ 0x04-python-more_data_structures/README.md
@@ -0,0 +1,1 @@
+in this folder we will disscuss about the dictionaries and other datastructues that are built in python
