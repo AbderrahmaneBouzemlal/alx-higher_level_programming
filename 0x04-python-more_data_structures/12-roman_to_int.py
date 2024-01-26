a0b1a69af15871e5d63ae46b25b02b8374716afe
AbderrahmaneBouzemlal <Abderrahmanebouzemlal@gmail.com>
2024-01-26 16:05:30 +0800

    Added a file

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_number):
     roman = {
         "I": 1,
-        "V": 5,    
+        "V": 5,
         "X": 10,
         "L": 50,
         "C": 100,
@@ -15,7 +15,7 @@ def roman_to_int(roman_number):
     number = 0
     for i in range(len(roman_number)-1):
         if roman[roman_number[i]] < roman[roman_number[(i+1)]]:
-            number-=roman[roman_number[i]]
+            number -= roman[roman_number[i]]
         else:
-            number+=roman[roman_number[i]]
+            number += roman[roman_number[i]]
     return number+roman[roman_number[-1]]
