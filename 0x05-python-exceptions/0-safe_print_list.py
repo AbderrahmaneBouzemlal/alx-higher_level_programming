3b2a778c96bbf4b4d7d658e066a02908b40b18a0
AbderrahmaneBouzemlal <Abderrahmanebouzemlal@gmail.com>
2024-02-05 17:44:19 +0800

    Added a file

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    real_count = 0
+    for element in range(x):
+        try:
+            print(my_list[element], end="")
+            real_count += 1
+        except IndexError:
+            print()
+            return real_count
+    print()
+    return real_count
+        
