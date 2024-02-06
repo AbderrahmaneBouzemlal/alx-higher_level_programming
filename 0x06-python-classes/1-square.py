c7af57b31e51467c706a05e081b52b64e4bc3d57
AbderrahmaneBouzemlal <Abderrahmanebouzemlal@gmail.com>
2024-02-06 20:19:02 +0800

    Added a file

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,6 +3,14 @@
 
 
 class Square:
+    """Represents a square
+    Attributes:
+        __size (int): size of a side of the square
+    """
     def __init__(self, size):
-        """ a private instance attribute"""
+        """Initializes a square
+        Args:
+            size (int): size of a side of the square
+        Returns: None
+        """
         self.__size = size
