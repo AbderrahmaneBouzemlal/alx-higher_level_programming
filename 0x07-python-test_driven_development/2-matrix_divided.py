29256c3862edca12c100438c9d8e26dedbfd8a94
AbderrahmaneBouzemlal <Abderrahmanebouzemlal@gmail.com>
2024-04-06 04:50:36 +0800

    Added a file

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""
+
+This is the "2-matrix_divided" module.
+
+The 2-matrix_divided module supplies one function, matrix_divided().
+
+"""
+def matrix_divided(matrix, div):
+
+    """ Return list of lists """
+    if type(div) != int and type(div) != float:
+        raise TypeError("div must be a number")
+    matrix_d = []
+    length = []
+    for ind, i in enumerate(matrix):
+        try:
+            matrix_v = [round(x/div, 2) for x in i]
+        except TypeError:
+            raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
+        except ZeroDivisionError:
+            raise ZeroDivisionError('division by zero')
+        
+        length.append(len(i) - len(matrix[ind - 1]))
+        matrix_d.append(matrix_v)
+    for b in length:
+        if b != 0:
+            raise TypeError("Each row of the matrix must have the same size")
+    return matrix_d
