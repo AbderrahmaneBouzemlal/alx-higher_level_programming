2241adc1b138f0ed2c77526e67e0406dae56d87b
AbderrahmaneBouzemlal <Abderrahmanebouzemlal@gmail.com>
2024-04-06 23:59:02 +0800

    Added a file

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
 """
-
 This is the "2-matrix_divided" module.
 
 The 2-matrix_divided module supplies one function, matrix_divided().
-
 """
+
+
 def matrix_divided(matrix, div):
 
     """ Return list of lists """
@@ -18,10 +18,11 @@ def matrix_divided(matrix, div):
         try:
             matrix_v = [round(x/div, 2) for x in i]
         except TypeError:
-            raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
+            raise TypeError('matrix must be a matrix (list of \
+                            lists) of integers/floats')
         except ZeroDivisionError:
             raise ZeroDivisionError('division by zero')
-        
+
         length.append(len(i) - len(matrix[ind - 1]))
         matrix_d.append(matrix_v)
     for b in length:
--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -2,10 +2,11 @@
 
 """
     This is the "4-print_square" module.
-    
+
     The 4-print_square module supplies one function, print_square().
 
 """
+
 
 def print_square(size):
     """Prints a square with the character #."""
