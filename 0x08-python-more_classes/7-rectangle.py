0ad1899813770ee29f3ce744b942ec9deae88da5
AbderrahmaneBouzemlal <Abderrahmanebouzemlal@gmail.com>
2024-04-09 08:19:11 +0800

    Added a file

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -59,7 +59,7 @@ class Rectangle:
         rect = ""
         if self.__width != 0 and self.__height != 0:
             rect += "\n".join(str(self.print_symbol) * self.__width
-                                for _ in range(self.__height))
+                              for _ in range(self.__height))
         return rect
 
     def __repr__(self):
