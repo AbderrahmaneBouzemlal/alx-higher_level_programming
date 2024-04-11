fb8afb705c085b3245a8515a2913e9ead7a6f91f
AbderrahmaneBouzemlal <Abderrahmanebouzemlal@gmail.com>
2024-04-11 09:34:14 +0800

    Added a file

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/path
+from typing import Any
+
+
+class LockedClass:
+    def __setattr__(self, name: str, value: Any) -> None:
+        if not hasattr(self, "first_name") and name != "first_name":
+            raise AttributeError("f'{slef.__class__.__name__}' object has no attribute '{name}'")
+        super().__setattr__(name, value)
