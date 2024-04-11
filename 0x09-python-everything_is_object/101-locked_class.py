0668aca01974454a2620faf83a43aa4fbd8d7509
AbderrahmaneBouzemlal <Abderrahmanebouzemlal@gmail.com>
2024-04-11 10:06:45 +0800

    Added a file

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -11,3 +11,4 @@ class LockedClass:
                 name != "first_name":
             raise AttributeError(f"'{self.__class__.__name__}'\
  object has no attribute '{name}'")
+        super().__setattr__(name, value)
