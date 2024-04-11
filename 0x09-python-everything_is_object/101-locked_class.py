467c4bc1a6e5d2061d8be7f1cfaf9b8fb1102dc1
AbderrahmaneBouzemlal <Abderrahmanebouzemlal@gmail.com>
2024-04-11 09:39:19 +0800

    Added a file

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"""this module has only one class called LockedClass"""
 
 
 class LockedClass:
+    """definning a method that allow to set attribute first_name automatically"""
     def __setattr__(self, name: str, value) -> None:
         if not hasattr(self, "first_name") and name != "first_name":
             raise AttributeError("f'{slef.__class__.__name__}' object has no attribute '{name}'")
