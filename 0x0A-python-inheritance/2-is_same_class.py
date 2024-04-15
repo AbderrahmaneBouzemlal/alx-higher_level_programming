c8d566ba9fc96016eb1ae791341ed44c61689806
AbderrahmaneBouzemlal <Abderrahmanebouzemlal@gmail.com>
2024-04-16 07:44:24 +0800

    Added a file

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""In this module there is only one function is_same_class"""
+
+
+def is_same_class(obj, a_class):
+    """returns a boolean expression"""
+    return type(obj) == a_class
