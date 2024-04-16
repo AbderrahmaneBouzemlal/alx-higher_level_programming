35406295b5f2e482135914483f10d09197d87809
AbderrahmaneBouzemlal <Abderrahmanebouzemlal@gmail.com>
2024-04-17 00:40:20 +0800

    Added a file

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -4,5 +4,13 @@ import json
 
 
 def from_json_string(my_str):
-    """returns a list from a json string"""
+    """ Function that reads from a file
+
+        Args:
+            my_str: a JSON string
+
+        Raises
+            Exception: when the my_str is not a JSON String
+
+    """
     return json.loads(my_str)
