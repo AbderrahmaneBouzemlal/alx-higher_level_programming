72ffdcb6b99185da3fc77084f1a68b65a8c663ab
AbderrahmaneBouzemlal <Abderrahmanebouzemlal@gmail.com>
2024-04-16 08:32:08 +0800

    Added a file

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This module contains a function called inherits_from
+"""
+
+
+def inherits_from(obj, a_class):
+    """return True if the object is an of a class inherited 
+    from specified class"""
+    return issubclass(obj, a_class)
