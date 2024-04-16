4a5e7014a760e248620971b71e6956133b3b1c27
AbderrahmaneBouzemlal <Abderrahmanebouzemlal@gmail.com>
2024-04-16 23:30:26 +0800

    Added a file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,7 +6,5 @@ def read_file(filename=""):
     """return the data in the file"""
     with open(filename, "r", encoding="utf-8") as file:
         for i in file:
-            if i == "\n":
-                continue
             print(i, end="")
         print()
