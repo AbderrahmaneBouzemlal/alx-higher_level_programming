537cab6c07fe514c3d3cc794455da72c8b82c357
AbderrahmaneBouzemlal <Abderrahmanebouzemlal@gmail.com>
2024-04-17 22:15:27 +0800

    Added a file

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This module have one function class_to_json
+"""
+import json
+
+
+def class_to_json(obj):
+    """
+    class_to_json function that converts from class to json
+        obj: the class instance
+    """
+    j_string = json.dumps(vars(obj), default=lambda x: x. __dict__)
+    return json.loads(j_string)
