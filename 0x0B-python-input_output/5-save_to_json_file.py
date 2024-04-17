7b9d28422ea11358d4a9c3a4353b52a1bfeda1a0
AbderrahmaneBouzemlal <Abderrahmanebouzemlal@gmail.com>
2024-04-17 19:10:52 +0800

    Added a file

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -5,7 +5,7 @@ a JSON representation
 import json
 
 
-def save_to_json_file(my_obj, filename):
+def save_to_json_file(my_obj, filename, append="w"):
     """ Function that writes an object to a text file
     by a JSON representation
 
@@ -17,5 +17,5 @@ def save_to_json_file(my_obj, filename):
         Exception: when the object can't be encoded
 
     """
-    with open(filename, 'w', encoding="utf-8") as f:
+    with open(filename, append, encoding="utf-8") as f:
         json.dump(my_obj, f)
--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,14 +3,21 @@
 this module contains two functions save_to_json_file
 and load_from_json_file
 """
+import sys
+import os
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-import sys
-FILE = "add_item.json"
 
 
 def main():
-    data = sys.argv[1]
-    save_to_json_file(data, FILE)
-    json_ob = load_from_json_file(FILE)
-    save_to_json_file(json_ob, FILE)
+    filename = 'add-item.json'
+
+    if os.path.exists(filename):
+        data = load_from_json_file(filename)
+    else:
+        data = []
+    data.extend(sys.argv[1:])
+    save_to_json_file(data, filename)
+
+if "__main__" == __name__:
+    main()
