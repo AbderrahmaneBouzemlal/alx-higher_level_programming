dfbd2d68b3281ada5082498bccfeee508dcb01e1
AbderrahmaneBouzemlal <Abderrahmanebouzemlal@gmail.com>
2024-04-17 11:13:47 +0800

    Added a file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,12 +4,13 @@ this module contains two functions save_to_json_file
 and load_from_json_file
 """
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-save_to_json_file = __import__('6-load_from_json_file').load_from_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 import sys
-import json
 FILE = "add_item.json"
 
 
 def main():
     data = sys.argv[1]
     save_to_json_file(data, FILE)
+    json_ob = load_from_json_file(FILE)
+    save_to_json_file(json_ob, FILE)
