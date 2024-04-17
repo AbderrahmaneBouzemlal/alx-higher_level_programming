c39317b23bbdbf5ac33a458b21029565d4787d28
AbderrahmaneBouzemlal <Abderrahmanebouzemlal@gmail.com>
2024-04-17 19:30:33 +0800

    Added a file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,14 +3,17 @@
 this module contains two functions save_to_json_file
 and load_from_json_file
 """
+
+
 import sys
-import os
+import os.path
+import json
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 filename = 'add-item.json'
-
 if os.path.isfile(filename):
     data = load_from_json_file(filename)
 else:
