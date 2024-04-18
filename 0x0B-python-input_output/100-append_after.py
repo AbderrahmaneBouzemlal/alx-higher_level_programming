7abbf3f3318c79a46a750fccabfa2639e692f428
AbderrahmaneBouzemlal <Abderrahmanebouzemlal@gmail.com>
2024-04-18 09:57:38 +0800

    Added a file

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module contains The function append_after"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    with open(filename, "a+", encoding= "utf-8") as file:
+        for line in file:
+            if search_string in line:
+                file.write(new_string)
