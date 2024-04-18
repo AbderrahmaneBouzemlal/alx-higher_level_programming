1921b35ad96311c4df4b80fe809056dacc01b050
AbderrahmaneBouzemlal <Abderrahmanebouzemlal@gmail.com>
2024-04-18 09:59:26 +0800

    Added a file

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -3,6 +3,7 @@
 
 
 def append_after(filename="", search_string="", new_string=""):
+    """appending to a file after a certain line"""
     with open(filename, "a+", encoding= "utf-8") as file:
         for line in file:
             if search_string in line:
