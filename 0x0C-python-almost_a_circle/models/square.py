c6501378a4b800397ee654bfe994d56bb7504eb6
AbderrahmaneBouzemlal <AbderrahmaneBouzemlal@gmail.com>
2024-04-23 02:24:42 +0800

    ./models/square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,6 +10,20 @@ class Square(Rectangle):
         super(Square, self).__init__(width=size, height=size, x=x, y=y, id=id)
         self.size = self.width
 
+    @property
+    def size(self):
+        """a getter for the width"""
+        return self.__width
+
+    @size.setter
+    def size(self, width):
+        """A setter for the width"""
+        if type(width) != int:
+            raise TypeError("width must be an integer")
+        if width <= 0:
+            raise ValueError("width must be > 0")
+        self.__size = width
+
     def __str__(self):
         """representation of class"""
         return f"[{Square.__name__}] ({self.id}\
