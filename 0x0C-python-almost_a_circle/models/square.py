1ed63efe665e8b2b58ce37ff6aeb3f1a539ca305
AbderrahmaneBouzemlal <AbderrahmaneBouzemlal@gmail.com>
2024-04-23 02:01:41 +0800

    added documentation

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -4,9 +4,13 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """Square inherits from Rectangle and base"""
     def __init__(self, size, x=0, y=0, id=None):
+        """initialize an object"""
         super(Square, self).__init__(width=size, height=size, x=x, y=y, id=id)
         self.size = self.width
 
     def __str__(self):
-        return f"[{Square.__name__}] ({self.id}) {self.x}/{self.y} - {self.size}"
+        """representation of class"""
+        return f"[{Square.__name__}] ({self.id}\
+) {self.x}/{self.y} - {self.size}"
