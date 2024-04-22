f88b23f7c0450fcfc2519d18c0a8802798b9fb52
AbderrahmaneBouzemlal <AbderrahmaneBouzemlal@gmail.com>
2024-04-23 01:47:54 +0800

    added the documentation

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,6 +6,7 @@ from models.base import Base
 class Rectangle(Base):
     """This class inherits from the Base class"""
     def __init__(self, width, height, x=0, y=0, id=None):
+        """initialize an object"""
         self.width = width
         self.height = height
         self.x = x
@@ -17,10 +18,12 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """a getter for the width"""
         return self.__width
     
     @width.setter
     def width(self, width):
+        """A setter for the width"""
         if type(width) != int:
             raise TypeError("width must be an integer")
         if width <= 0:
@@ -29,10 +32,12 @@ class Rectangle(Base):
 
     @property
     def height(self):
+        """a getter for the height"""
         return self.__height
     
     @height.setter
     def height(self, height):
+        """A setter for the height"""
         if type(height) != int:
             raise TypeError("height must be an integer")
         if height <= 0:
@@ -41,10 +46,12 @@ class Rectangle(Base):
 
     @property
     def x(self):
+        """a getter for the x"""
         return self.__x
 
     @x.setter
     def x(self, x):
+        """A setter for the x"""
         if type(x) != int:
             raise TypeError("x must be an integer")
         if x < 0:
@@ -53,10 +60,12 @@ class Rectangle(Base):
 
     @property
     def y(self):
+        """a getter for the y"""
         return self.__y
 
     @y.setter
     def y(self, y):
+        """A setter for the y"""
         if type(y) != int:
             raise TypeError("y must be an integer")
         if y < 0:
@@ -64,6 +73,7 @@ class Rectangle(Base):
         self.__y = y
 
     def update(self, *args, **kwargs):
+        """update the class attributes"""
         length = len(args)
 
         if length > 0:
@@ -92,13 +102,16 @@ class Rectangle(Base):
                     self.y = v
 
     def area(self):
+        """calculate the area of the Rectangle"""
         return self.width * self.height
 
     def display(self):
+        """display the rectangle"""
         rect =  "" + "\n" * self.y
         rect += "\n".join(" " * self.x + "#" * self.__width
                           for _ in range(self.__height))
         print(rect)
 
     def __str__(self):
+        """the string representation"""
         return f"[{Rectangle.__name__}] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
