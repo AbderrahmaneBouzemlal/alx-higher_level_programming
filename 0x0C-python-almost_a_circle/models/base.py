589dbc1c72d2ce6cce1ed91c4b5820280dcc527f
AbderrahmaneBouzemlal <AbderrahmaneBouzemlal@gmail.com>
2024-04-24 22:41:21 +0800

    added some style

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
-""" In this module we are going to do Base class"""
+""" 
+In this module we are going to do Base class
+"""
 import json
 
 
 class Base:
-    """A class called base"""
+    """
+    A class called base
+    """
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """intialize an object of the class Base"""
+        """
+        intialize an object of the class Base
+        """
         if id is not None:
             self.id = id
         else:
