e6196212497ca6a971a0a3619ac17e0828d110c3
AbderrahmaneBouzemlal <AbderrahmaneBouzemlal@gmail.com>
2024-04-25 21:32:55 +0800

    fixed a bug

--- 0x0C-python-almost_a_circle/main.py
+++ 0x0C-python-almost_a_circle/main.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     r1 = Rectangle(3, 5, 1)
     r1_dictionary = r1.to_dictionary()
     r2 = Rectangle.create(**r1_dictionary)
-    r3 = Square.create(**{ 'size': 2, 'x': 1, 'y': 3, 'id': 89 })
+    r3 = Rectangle.create(**{ 'width': 2, 'height': 3, 'x': 12, 'y': 1, 'id': 89 })
     print(r1)
     print(r2)
     print(r3)
