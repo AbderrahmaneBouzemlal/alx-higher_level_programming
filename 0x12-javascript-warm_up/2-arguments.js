825511caae68987e9b874bad57b0d407721d39e2
AbderrahmaneBouzemlal <AbderrahmaneBouzemlal@gmail.com>
2024-06-19 00:10:39 +0800

    added another file

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const { argv } = require('node:process');
+const argc = argv.length
+if (argc < 3){
+    console.log('No Argument');
+}
+else if (argc == 3) {
+    console.log('Argument found');
+}
+else {
+    console.log('Arguments found');
+}
