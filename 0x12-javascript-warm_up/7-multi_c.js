97385fa1d0199cac41365cb28e33c5001f9cc4ac
AbderrahmaneBouzemlal <AbderrahmaneBouzemlal@gmail.com>
2024-06-19 08:51:49 +0800

    fixed some bugs

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
-x = argv[2];
-argc = argv.length
+const x = argv[2];
+const argc = argv.length;
 if (argc < 3) {
-    console.log('Missing number of occurrences');
+  console.log('Missing number of occurrences');
 } else if (argc === 3 && !(isNaN(x)) && x > 0) {
-    for (let i = 0; i < x; i++) {
-        console.log('C is fun');
-    }
-} else {}
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
+}
