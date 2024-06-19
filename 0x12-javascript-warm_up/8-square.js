7c6fd8b878b2aafe6bc3aa10185bccb5d659406a
AbderrahmaneBouzemlal <AbderrahmaneBouzemlal@gmail.com>
2024-06-19 09:38:38 +0800

    added a file

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const { argv } = require("node:process");
+const x = argv[2];
+const argc = argv.length;
+
+if (argc < 3 || isNaN(x)) {
+    console.log('Missing size');
+}
+else if (!(isNaN(x)) && x > 0) {
+    for (let i = 0; i < x; i++) {
+        console.log('X'.repeat(x));
+    }
+}
