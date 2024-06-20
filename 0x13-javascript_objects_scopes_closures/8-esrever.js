2cf623ecb489393c99076329df2ca8186cd942f3
AbderrahmaneBouzemlal <AbderrahmaneBouzemlal@gmail.com>
2024-06-20 10:27:51 +0800

    added another file

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  let reversed = Array(list.length);
+  const reversed = Array(list.length);
   for (let i = 0; i < list.length; i++) {
     reversed[i] = list[list.length - 1 - i];
   }
   return reversed;
-}
+};
