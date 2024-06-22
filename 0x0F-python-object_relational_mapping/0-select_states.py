4deac3cc4f7cb0abefa277faf84791561e583762
AbderrahmaneBouzemlal <AbderrahmaneBouzemlal@gmail.com>
2024-06-22 23:29:31 +0800

    the first commit in this file

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+
+def list_states(username, password, dbname):
+    """ List all the states"""
+    # Connect to the MySQL server
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=dbname)
+
+    # Create a cursor object
+    cur = db.cursor()
+
+    # Execute the SQL query
+    cur.execute("SELECT id, name FROM states ORDER BY id ASC")
+
+    # Fetch all the rows in a list of lists.
+    rows = cur.fetchall()
+
+    # Print the results
+    for row in rows:
+        print(row)
+
+    # Close the cursor and database connection
+    cur.close()
+    db.close()
+
+if __name__ == "__main__":
+    # Pass the arguments (username, password, dbname)
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dbname = sys.argv[3]
+
+    # Call the function to list states
+    list_states(username, password, dbname)
