327badb92faac3a8ac7be3ff54960062c390b3e8
AbderrahmaneBouzemlal <AbderrahmaneBouzemlal@gmail.com>
2024-06-23 11:35:47 +0800

    fixed a bug

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+This script takes in an argument and
+displays all values in the states
+where `name` matches the argument
+from the database `hbtn_0e_0_usa`
+Without possibility of sql injection threat.
+"""
+
+import sys
+import MySQLdb
+
+
+def list_states(username, password, dbname, arg):
+    """ List all the states"""
+    # Connect to the MySQL server
+    db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                         passwd=password, db=dbname)
+
+    # Create a cursor object
+    cur = db.cursor()
+
+    # Execute the SQL query
+    cur.execute("""
+                SELECT id, name FROM states
+                WHERE name LIKE BINARY %s
+                ORDER BY states.id ASC""", (arg,))
+
+    # Fetch all the rows in a list of lists.
+    rows = cur.fetchall()
+
+    # Print the results
+    for row in rows:
+        if arg in row:
+            print(row)
+
+    # Close the cursor and database connection
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    # Pass the arguments (username, password, dbname)
+    USERNAME = sys.argv[1]
+    PASSWORD = sys.argv[2]
+    DBNAME = sys.argv[3]
+    ARG = sys.argv[4]
+
+    # Call the function to list states
+    list_states(USERNAME, PASSWORD, DBNAME, ARG)
