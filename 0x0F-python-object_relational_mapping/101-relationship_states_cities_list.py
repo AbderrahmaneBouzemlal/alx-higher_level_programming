cb3601b028d64513e5593e876d5f0d0e799bdb52
AbderrahmaneBouzemlal <AbderrahmaneBouzemlal@gmail.com>
2024-06-29 02:33:09 +0800

    finished a task

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,36 +1,9 @@
-#!/usr/bin/python3
-"""
-This script lists all State objects,
-and corresponding City objects,
-contained in the database hbtn_0e_101_usa
-"""
+states = session.query(State).order_by(State.id).all()
 
-from sys import argv
-from relationship_state import Base, State
-from relationship_city import City
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+        for state in states:
+            print(f"{state.id}: {state.name}")
+            for city in state.cities:
+                print(f"    {city.id}: {city.name}")
 
-
-if __name__ == "__main__":
-    """
-    Access the database and get the states
-    and the corresponding City
-    """
-    db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-        argv[1], argv[2], argv[3])
-    engine = create_engine(db_uri)
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-
-    session = Session()
-
-    states = session.query(State).order_by(State.id).all()
-
-    for state in states:
-        print(f"{state.id}: {state.name}")
-        for city in state.cities:
-            print(f"    {city.id}: {city.name}")
-
-    session.commit()
-    session.close()
+        session.commit()
+        
