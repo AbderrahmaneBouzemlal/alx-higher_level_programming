b75d7481adfc4c584a8be0e1770349f5d403c950
AbderrahmaneBouzemlal <AbderrahmaneBouzemlal@gmail.com>
2024-06-28 23:53:30 +0800

    documentation

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -3,6 +3,7 @@
 creates the State "California" with the city "San Francisco"
 from the database 'hbtn_0e_100_usa'
 """
+
 from sys import argv
 from model_state import Base, State
 from model_city import City
@@ -11,10 +12,6 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
-	"""
-    Access to the database and add a state California
-    and a city san francisco to the database.
-    """
     db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         argv[1], argv[2], argv[3])
     engine = create_engine(db_uri)
