c423e9a3370328215d05bb24edab481f585e2780
AbderrahmaneBouzemlal <AbderrahmaneBouzemlal@gmail.com>
2024-07-05 02:52:53 +0800

    added a file

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-
+"""This module have only one function find_peak"""
 
 def find_peak(list_int):
+	"""returns the biggest number"""
 	maxi = 0
 	if len(list_int) == 0:
 		return None
