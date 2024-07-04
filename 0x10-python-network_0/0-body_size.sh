c33ef149802445a4633c6e085c1baebbde03ee3b
AbderrahmaneBouzemlal <AbderrahmaneBouzemlal@gmail.com>
2024-07-05 00:31:43 +0800

    added the newline

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-readarray -d ' : ' -t strarr <<< $(curl -sI $1 | grep "content-length"); printf "${strarr[1]}"
+readarray -d ' : ' -t strarr <<< "$(curl -sI "$1" | grep 'content-length')"; echo -n "${strarr[1]}"
 
