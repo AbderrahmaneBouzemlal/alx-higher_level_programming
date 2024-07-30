47914069ad787a9098cb507f59e5a0fe1e97936c
AbderrahmaneBouzemlal <AbderrahmaneBouzemlal@gmail.com>
2024-07-30 15:00:25 +0800

    added some style

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,9 +3,9 @@ const { argv } = require('node:process');
 const fs = require('node:fs');
 
 fs.readFile(`${argv[2]}`, 'utf8', (err, data) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    console.log(data);
-})
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
