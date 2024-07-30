8513f5c33869e99d7a81bd174cbc49c04e3593b6
AbderrahmaneBouzemlal <AbderrahmaneBouzemlal@gmail.com>
2024-07-30 15:15:10 +0800

    added a file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const { argv } = require('node:process');
+const fs = require('node:fs');
+
+fs.writeFile(`${argv[2]}`, `${argv[3]}`, 'utf8', (err) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+});
